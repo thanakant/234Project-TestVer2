539f27212dfea497c9dd27130bdf94d1ac9f90e9
Unknown <chanaphasiri@gmail.com>
2018-05-08 18:04:36 +0700

    Record check user and password test case

--- Project/Scripts/Check username and password test case/Script1525777320046.groovy
+++ Project/Scripts/Check username and password test case/Script1525777320046.groovy
@@ -18,4 +18,19 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://34.223.223.31:8091/')
+
+WebUI.setText(findTestObject('Page_ProjectBackend (4)/input_username'), 'Hello user')
+
+WebUI.setText(findTestObject('Page_ProjectBackend (4)/input_password'), 'user')
+
+WebUI.click(findTestObject('Page_ProjectBackend (4)/button_Login'))
+
+WebUI.click(findTestObject('Page_ProjectBackend (4)/label_Usernamepassword is inco'))
+
+WebUI.closeBrowser()
 
